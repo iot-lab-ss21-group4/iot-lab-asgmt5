b150df94cfee2c8ba366501d439f361f19358108
Maximilian Wiesholler <maximilian.wiesholler@tum.de>
2021-06-06 15:38:07 +0200

    Changes to LSTM but still no good results

--- lstm_model.py
+++ lstm_model.py
@@ -3,14 +3,14 @@ import os
 from typing import Optional
 
 import matplotlib.pyplot as plt
-import numpy
 import numpy as np
 import pandas as pd
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
-from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
+from ray import tune
+from ray.tune.integration.pytorch_lightning import TuneReportCallback
 from sklearn.preprocessing import MinMaxScaler
 from torch.utils.data import DataLoader, Dataset
 
@@ -39,27 +39,36 @@ class StudentCountPredictor(pl.LightningModule):
     y_column = [UNIVARIATE_DATA_COLUMN]
     useless_rows = 1
 
-    def __init__(self, data_path, is_data_csv, n_features=5, n_hidden=32, n_layers=1, n_outputs=1, n_batch_size=8, seq_len=4):
+    def __init__(self, config, data_path, is_data_csv):
         super().__init__()
 
         self.data_path = data_path
         self.is_data_csv = is_data_csv
 
-        self.n_hidden = n_hidden
-        self.n_layers = n_layers
+        self.lr = config["lr"]
+
+        self.n_features = config["n_features"]
+        self.n_hidden = config["n_hidden"]
+        self.n_layers = config["n_layers"]
+        self.n_outputs = config["n_outputs"]
+
+        self.batch_size = config["batch_size"]
+        self.seq_len = config["seq_len"]
+
         self.lstm = nn.LSTM(
-            input_size=n_features,
-            hidden_size=n_hidden,
+            input_size=self.n_features,
+            hidden_size=self.n_hidden,
             batch_first=True,  # batch of sequences
-            num_layers=n_layers,  # stack LSTMs on top of each other
+            num_layers=self.n_layers,  # stack LSTMs on top of each other
             # dropout = 0.2
         )
-        self.regressor = nn.Linear(n_hidden, n_outputs)
-
+        self.relu = nn.ReLU()
+        self.linear = nn.Linear(self.n_hidden, self.n_hidden)
+        self.regressor = nn.Linear(self.n_hidden, self.n_outputs)
         self.criterion = nn.MSELoss()
-        self.batch_size = n_batch_size
-        self.seq_len = seq_len
-        self.test_results = None
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
 
     def forward(self, x, labels=None):
         # load to device
@@ -69,6 +78,8 @@ class StudentCountPredictor(pl.LightningModule):
         lstm_out, (h_n, c_n) = self.lstm(x)
         # get the output of the last hidden layer
         out = lstm_out[:, -1]
+        out = self.linear(out)
+        out = self.relu(out)
         # apply linear layer
         return self.regressor(out)
 
@@ -103,9 +114,6 @@ class StudentCountPredictor(pl.LightningModule):
         self.log("test_loss", avg_loss)
         return {"test_loss": avg_loss}
 
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=0.0001)
-
     def fit_scaler_to_data(self, ts: pd.DataFrame):
         self.scaler_features = MinMaxScaler(feature_range=(0, 1))
         self.scaler_features.fit(ts[self.x_columns])
@@ -115,6 +123,10 @@ class StudentCountPredictor(pl.LightningModule):
     def scale_data(self, ts: pd.DataFrame):
         ts.loc[:, self.x_columns] = self.scaler_features.transform(ts.loc[:, self.x_columns])
         ts.loc[:, self.y_column] = self.scaler_count.transform(ts.loc[:, self.y_column])
+
+    def inverse_data(self, ts: pd.DataFrame):
+        ts.loc[:, self.x_columns] = self.scaler_features.inverse_transform(ts.loc[:, self.x_columns])
+        ts.loc[:, self.y_column] = self.scaler_count.inverse_transform(ts.loc[:, self.y_column])
 
     def prepare_data(self, stage=None):
 
@@ -164,42 +176,43 @@ class StudentCountPredictor(pl.LightningModule):
         return DataLoader(self.dataset["test"], batch_size=self.batch_size)
 
     def predict_single(self, sequence: torch.Tensor) -> float:
-        count = self(sequence)
-        return count.item()
+        return self(sequence).item()
 
-    def predict(self, ts: pd.DataFrame) -> numpy.ndarray:
+    def predict(self, ts: pd.DataFrame, update_last_count=True):
+
         self.scale_data(ts)
 
-        predictions = []
-
         # first seq_len + 1 counts are still known
-        for i in range(self.seq_len + 1):
-            predictions.append(ts[self.y_column].iloc[i].values[0])
-
-        last_count = ts[self.y_column].iloc[self.seq_len].values[0]
+        for i in range(self.useless_rows, self.seq_len + 1):
+            if update_last_count:
+                ts.loc[ts.index[i], "last_count"] = ts.loc[ts.index[i - 1], "count"]
 
         # window starts at second count because first always has NaN data
         for i in range(self.useless_rows, len(ts.index) - self.seq_len):
 
             # update last count
-            ts["last_count"][i + self.seq_len - 1] = last_count
+            if update_last_count:
+                last_count = ts.loc[ts.index[i + self.seq_len - 1], "count"]
+                ts.loc[ts.index[i + self.seq_len], "last_count"] = last_count
 
             # get sequence and convert to tensor
-            X_sequence = ts[self.x_columns][i : i + self.seq_len].to_numpy()
+            X_sequence = ts.loc[ts.index[i] : ts.index[i + self.seq_len], self.x_columns].to_numpy()
             X_sequence = torch.tensor(X_sequence).float()
             X_sequence = torch.unsqueeze(X_sequence, 0)
 
-            last_count = self.predict_single(X_sequence)
-            predictions.append(last_count)
+            # predict
+            value = self.predict_single(X_sequence)
+            ts.loc[ts.index[i + self.seq_len], "count"] = value
 
-        predictions = self.scaler_count.inverse_transform(numpy.asarray(predictions).reshape(-1, 1))
-        predictions = numpy.round(predictions).astype(int).reshape(-1)
-        return predictions
+        self.inverse_data(ts)
+        ts["count"] = ts["count"].round(decimals=0)
+        ts["count"] = ts["count"].astype(np.int)
 
     def plot_after_train(self):
         _, _, ts, _ = load_data_with_features(self.data_path, self.is_data_csv)
         all_target = ts[self.y_column]
-        all_pred = pd.Series(self.predict(ts), index=all_target.index, name="predicted_count")
+        self.predict(ts, update_last_count=True)
+        all_pred = pd.Series(ts[self.y_column].values.reshape(-1), index=all_target.index, name="predicted_count")
         all_target.index.freq = None
         all_pred.index.freq = None
         all_target.plot(legend=True)
@@ -207,27 +220,71 @@ class StudentCountPredictor(pl.LightningModule):
         plt.show()
 
 
-def train(args: argparse.Namespace) -> StudentCountPredictor:
+def train_analysis(config, data_path, is_csv_data, num_epochs):
+    model = StudentCountPredictor(config, data_path, is_csv_data)
+    tune_callback = TuneReportCallback({"loss": "val_loss"}, on="validation_end")
+    trainer = pl.Trainer(
+        logger=TensorBoardLogger("tensorboard_logs", name="count-students"),
+        callbacks=[tune_callback],
+        max_epochs=num_epochs,
+        progress_bar_refresh_rate=0,
+    )
+    trainer.fit(model)
+
+
+def train_with_ray_tune(args: argparse.Namespace):
+
+    # services.get_node_ip_address = lambda: '127.0.0.1'
+    # ray.init(local_mode=True, include_dashboard=False, num_cpus=1, logging_level=logging.DEBUG, num_redis_shards=0)
+
+    analysis = tune.run(
+        tune.with_parameters(train_analysis, data_path=args.training_data_path, is_data_csv=args.is_data_csv, num_epochs=10),
+        metric="loss",
+        mode="min",
+        config={
+            "n_features": 5,
+            "n_layers": 1,
+            "n_hidden": tune.choice([8, 16, 32]),
+            "n_outputs": 1,
+            "lr": tune.loguniform(1e-4, 1e-1),
+            "batch_size": tune.choice([8, 16, 32]),
+            "seq_len": tune.choice([2, 4, 8]),
+        },
+        num_samples=10,
+        progress_reporter=tune.CLIReporter(
+            parameter_columns=["n_hidden", "lr", "batch_size", "seq_len"], metric_columns=["loss", "training_iteration"]
+        ),
+        name="tune_scp",
+    )
+    print(analysis.best_config)
+
+
+def train_standard(args: argparse.Namespace):
     print("GPU available: " + str(torch.cuda.is_available()))
 
     N_EPOCHS = 10
 
-    checkpoint_callback = ModelCheckpoint(
-        dirpath=args.model_directory, filename=args.model_name, save_top_k=1, verbose=True, monitor="val_loss", mode="min"
-    )
     logger = TensorBoardLogger("tensorboard_logs", name="count-students")
 
-    model = StudentCountPredictor(args.training_data_path, args.is_data_csv)
-    trainer = pl.Trainer(logger=logger, callbacks=[checkpoint_callback], max_epochs=N_EPOCHS, progress_bar_refresh_rate=30)
+    config = {"n_features": 5, "n_layers": 2, "n_hidden": 32, "n_outputs": 1, "lr": 0.001, "batch_size": 4, "seq_len": 4}
+
+    model = StudentCountPredictor(config, args.training_data_path, args.is_data_csv)
+
+    trainer = pl.Trainer(logger=logger, max_epochs=N_EPOCHS, progress_bar_refresh_rate=30)
+
     trainer.fit(model)
     if args.plot_fitted_model:
         model.plot_after_train()
     trainer.test(model)
 
-    end_model_path = os.path.join(args.model_directory, args.model_name)
-    torch.save(model, end_model_path)
+    torch.save(model, args.model_path)
 
-    return model
+
+def train(args: argparse.Namespace):
+    if args.using_ray:
+        train_with_ray_tune(args)
+    else:
+        train_standard(args)
 
 
 def prepare_pred_input(pred_time: int, pred_data_path: str, dataset_path: str):
@@ -241,9 +298,16 @@ def predict(args: argparse.Namespace, model: Optional[StudentCountPredictor] = N
         model: StudentCountPredictor = torch.load(args.model_path)
     model.eval()
 
-    predictions = model.predict(ts)
+    model.predict(ts)
+    predictions = ts[model.y_column].values.reshape(-1)
 
     all_pred = pd.Series(predictions, index=ts.index, name=UNIVARIATE_DATA_COLUMN)
+
+    if args.plot_predicted_model:
+        all_pred.index.freq = None
+        all_pred.plot(legend=True, linestyle="dotted")
+        plt.show()
+
     all_pred.to_csv(args.pred_out_path, index=False)
     return args.pred_out_path
 
@@ -254,10 +318,8 @@ def periodic_forecast(args: argparse.Namespace):
 
 
 def add_arguments(parser: argparse.ArgumentParser):
-    parser.add_argument("--gpus", default=None)
     subparser = parser.add_subparsers(title="Subcommands")
-    # Add command line arguments for 'train' subcommand.
-    train_parser = subparser.add_parser("train", help="Subcommand to train the model.")
+    train_parser = subparser.add_parser("train")
     train_parser.add_argument(
         "--training-data-path",
         type=str,
@@ -266,38 +328,47 @@ def add_arguments(parser: argparse.ArgumentParser):
     )
     train_parser.add_argument(
         "--is-data-csv",
-        type=bool,
         action="store_true",
         help="Optional flag for telling if the data file is '.csv'. Otherwise it is assumed to be '.db' (sqlite).",
     )
+    # TODO: using raytune
+    # train_parser.add_argument(
+    #     "--study-name",
+    #     type=str,
+    #     default="lr_fit",
+    #     help="Optional study name for the optuna study during hyperparameter optimization.",
+    # )
+    # train_parser.add_argument("--study-name", type=str, default="lr_fit", help="Optional study name for the optuna study.")
+    # train_parser.add_argument(
+    #     "--storage-url",
+    #     type=str,
+    #     default="sqlite:///hyperparams/lstm_params.db",
+    #     help="URL to the database storage for the optuna study.",
+    # )
+    # train_parser.add_argument(
+    #     "--model-name",
+    #     type=str,
+    #     default="lstm-model",
+    #     help="Name of the new model to be saved.",
+    # )
+    # train_parser.add_argument(
+    #     "--model-directory",
+    #     type=str,
+    #     default="checkpoints",
+    #     help="(Relative) directory of the model that will be saved.",
+    # )
     train_parser.add_argument(
-        "--study-name",
+        "--model-path",
         type=str,
-        default="lr_fit",
-        help="Optional study name for the optuna study during hyperparameter optimization.",
-    )
-    train_parser.add_argument(
-        "--storage-url",
-        type=str,
-        default="sqlite:///hyperparams/lstm_params.db",
-        help="URL to the database storage for the optuna study.",
-    )
-    train_parser.add_argument(
-        "--model-name",
-        type=str,
-        default="lstm-model",
-        help="Name of the new model to be saved.",
-    )
-    train_parser.add_argument(
-        "--model-directory",
-        type=str,
-        default="checkpoints",
-        help="(Relative) directory of the model that will be saved.",
+        default=os.path.join("checkpoints", "model.lstm"),
+        help="Path for the new model to be saved.",
     )
     train_parser.add_argument(
         "--plot-fitted-model", action="store_true", help="Optional flag for plotting the fitted model predictions."
     )
+    train_parser.add_argument("--using-ray", action="store_true", help="Optional flag for using training with RayTune.")
     train_parser.set_defaults(func=train)
+
     # Add command line arguments for 'pred' subcommand.
     pred_parser = subparser.add_parser("pred", help="Subcommand to predict given the saved model.")
     pred_parser.add_argument(
@@ -309,7 +380,7 @@ def add_arguments(parser: argparse.ArgumentParser):
     pred_parser.add_argument(
         "--model-path",
         type=str,
-        default=os.path.join("checkpoints", "model.ckpt"),
+        default=os.path.join("checkpoints", "model.lstm"),
         help="Path for the model to be loaded.",
     )
     pred_parser.add_argument(
@@ -318,7 +389,11 @@ def add_arguments(parser: argparse.ArgumentParser):
         default=os.path.join("datasets", "real_pred_lstm_out.csv"),
         help="Path to the '.csv' file to write the prediction.",
     )
+    pred_parser.add_argument(
+        "--plot-predicted-model", action="store_true", help="Optional flag for plotting the predicted model."
+    )
     pred_parser.set_defaults(func=predict)
+
     # Add command line arguments for 'periodic_forecast' subcommand.
     periodic_forecast_parser = subparser.add_parser(
         "periodic_forecast", help="Subcommand for doing periodic forecast and publishing it using MQTT."
@@ -331,7 +406,6 @@ def add_arguments(parser: argparse.ArgumentParser):
     )
     periodic_forecast_parser.add_argument(
         "--is-data-csv",
-        type=bool,
         action="store_true",
         help="Optional flag for telling if the data file is '.csv'. Otherwise it is assumed to be '.db' (sqlite).",
     )
